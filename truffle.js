350b568399e29dea633b080794d48b0b49d8d3fc
zibiaowu <547979729@qq.com>
2017-01-01 22:00:54 +0800

    文件转移回调；目录结构修改

--- truffle.js
+++ truffle.js
@@ -1,24 +1,8 @@
 module.exports = {
   build: {
     "index.html": "index.html",
-	"app.js": [
-      "javascripts/app.js"
-    ],
-	"spark-MD5.js":[
-	   "javascripts/spark-MD5.js"
-	],
-	"jquery-3.1.1.min.js":[
-	   "javascripts/jquery-3.1.1.min.js"
-	],
-	"bootstrap.min.js":[
-	   "javascripts/bootstrap.min.js"
-	],
-    "main.css": [
-      "stylesheets/main.css"
-    ],
-	"bootstrap.min.css":[
-	  "stylesheets/bootstrap.min.css"
-	],
+	"js/":"js/",
+    "css/": "css/",
     "images/": "images/",
 	"fonts/": "fonts/"
   },
